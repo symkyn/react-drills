57b214bb95d46a8352362e419c3fbb83fb88d17e
Simon <everett.blake@gmail.com>
2018-05-19 12:39:43 -0600

    step 3 done

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,19 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state={
+      userInput: '',
+      array: ['green', 'red', 'yellow', 'purple', 'blue', 'orange'],
+      filteredArray: ['green', 'red', 'yellow', 'purple', 'blue', 'orange'],
+    }
+  }
+
   render() {
+
+    let arrayDisplay = this.state.filteredArray.map((e,i) => (<h3 key={`list ${i}`}>{e}</h3>));
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input 
+            onChange={(event) => this.handleChange(event.target.value)}
+            value={this.state.userInput} />
+      <div>
+          {arrayDisplay}  
+      </div>
       </div>
     );
   }
+
+  handleChange(value){
+    const myArray = this.state.array;
+    const newArray = [];
+
+    for(var i = 0; i < myArray.length; i++) {
+      if(myArray[i].includes(value)){
+        newArray.push(myArray[i]);
+      }
+    }
+
+    this.setState({
+      filteredArray: newArray,
+      userInput: value,
+    })
+  }
+ 
+
 }
 
 export default App;
