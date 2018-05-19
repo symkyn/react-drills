47bf4388636b07b9a3f0d3104b2e8faefcc53951
Simon <everett.blake@gmail.com>
2018-05-19 13:45:53 -0600

    done with number 5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Image from "./components/Image";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Image myImage={
+            'https://cf.geekdo-images.com/original/img/l4dwCcl4Vz0r4TD-2RgQ_uoHvWM=/0x0/pic3469216.jpg'} />>
       </div>
     );
   }
---
+++ app-5/src/components/Image.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+ function Image(props){
+        return (
+            <img src={props.myImage} alt="" />
+        )
+    }
+
+
+export default Image;
