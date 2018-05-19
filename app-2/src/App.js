fa1e71c9a6eb5f497b5340d5933ab0bd6274e051
Simon <everett.blake@gmail.com>
2018-05-19 11:45:30 -0600

    2 done

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+     super() 
+
+     this.state={
+       list: ['green', 'blue', 'red', 'pink', 'yellow']
+     }
+
+     
+  }
+
+  
+
   render() {
+
+    var listItems = this.state.list.map((e,i) => (<h2 key={`list-item ${i}`}> {e} </h2>))
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {listItems}
       </div>
     );
   }
