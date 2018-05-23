273d4c6ca83ccdb5e9dd5f415aa1895a9896f99c
Simon <everett.blake@gmail.com>
2018-05-22 20:51:20 -0600

    6 done

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,21 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Todo from './components/Todo';
 
 class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state={
+      taskList: [],
+      userInput: '',
+    }
+
+    this.addTask = this.addTask.bind( this );
+  }
+
   render() {
+    let completeTaskList = this.state.taskList.map((e, i) => {
+      return (
+        <Todo key={i} task={e} />
+      )
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h1>My to-do list:</h1>
+          <div>
+            <input onChange={(e) => this.handleChange(e)} value={this.state.userInput} />
+            <button onClick={this.addTask}>add</button>
+          </div>
+      <br />
+      { completeTaskList }
       </div>
     );
   }
+
+  handleChange(e) {
+    this.setState({
+      userInput: e.target.value,
+    })
+  }
+
+  addTask() {
+    let newTask = this.state.userInput
+    this.setState({
+      taskList: [...this.state.taskList, newTask],
+      userInput: '',
+    })
+  }
+
 }
 
 export default App;
